2eab2ea91963cc2af87ea34ebf419f91007ad20c
truong-lx <truonglx@allinbyte.io>
2025-11-11 23:18:54 +0700

    updated

--- src/modules/product/schemas/product.schema.ts
+++ src/modules/product/schemas/product.schema.ts
@@ -1,5 +1,29 @@
 import { Prop, SchemaFactory } from '@nestjs/mongoose';
+export class ProductOption {
+  name: string;
+  values: [string];
+}
+export class VariantOption {
+  name: string;
+  value: string;
+}
+export class ProductImage {
+  src: string;
+  alt: string;
+}
 
+export class ProductVariant {
+  title: string;
+  sku: string;
+  price: string;
+  compare_at_price: string;
+  option1?: string;
+  option2?: string;
+  option3?: string;
+  image: ProductImage;
+  created_at: Date;
+  updated_at: Date;
+}
 export class Product {
   @Prop({ trim: true, type: String })
   title;
@@ -13,8 +37,20 @@ export class Product {
   @Prop({ type: String })
   handle;
 
-  @Prop({ type: [String] })
+  @Prop({ type: [String], default: [] })
   tags;
+
+  @Prop({ type: [ProductImage], default: [] })
+  images: ProductImage[];
+
+  @Prop({
+    type: Array,
+    default: [],
+  })
+  options: ProductOption[];
+
+  @Prop({ type: [ProductVariant], default: [] })
+  variants: ProductVariant[];
 
   @Prop({ type: Boolean, default: false })
   deleted;
